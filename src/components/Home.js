74d6a522a75214e5e55ace0570b37b9e22d4e576
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-10-02 21:45:16 +0300

    Modified Home.js to have NavBar and styled the nav bar in the css file

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <nav className="navBar">
+        <ul>
+          <a href="">HOME</a>
+          <a href="">ABOUT</a>
+          <a href="">SERVICES</a>
+          <a href="">CONTACT US</a>
+        </ul>
+      </nav>
+
+      <div className="carousel"></div>
+    </div>
+  );
+}
+
+export default Home;
