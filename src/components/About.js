bb450b18b9f77ad1cdfdf537c4ea41a26c0992e6
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-10-09 22:23:20 +0300

    Modified About component to contain data to be displayed

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>Geoda Greens. Your Dilligent & Honest Partner in Farming Practices</h1>
+        <h2>Cultivating a Sustainable Farming Future</h2> 
+
+        GEODA GREEN SUPPLIERS LTD.
+
+        <div id="content">
+          <p> 
+            Geoda Green Suppliers Ltd. is a comapny that is dedicated to providing farmers with high quality, durable greenhouses and farming materials that suit different climatic conditions both in Kenya and many African countries.
+
+            Through services provided, farmers who over the years were dependant on common farming methods, have discovered that they can farm on a relatively small piece of land controlled under a greenhouse and give them higher returns.This therefore has helped both large and small scale farmers curb frequemt farming challenges hence increasing farm produce. 
+             </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
